3490b7bde42d9dee71bf9db7165f0801a263bb8d
Rahul Changlani <rahul.changlani@gmail.com>
2017-07-08 18:36:18 +0530

    heave

---
+++ ALL LEVELS/Format_a_string_of_names_like_Bart_Lisa_&_Maggie.js
@@ -0,0 +1,59 @@
+// best sol
+
+function list(names){
+  return names.reduce(function(prev, current, index, array){
+    if (index === 0){
+      return current.name;
+    }
+    else if (index === array.length - 1){
+      return prev + ' & ' + current.name;
+    } 
+    else {
+      return prev + ', ' + current.name;
+    }
+  }, '');
+ }
+
+ // sol 2
+
+function list(arr) {
+  return arr.map(function(person) { return person.name; }) // Use only .name
+  .join(", ") // Join with ", "
+  .replace(/, (?!.*, )/, " & "); // Replace last ", " with " & "
+}
+
+
+/*
+
+Given: an array containing hashes of names
+
+Return: a string formatted as a list of names separated by commas except for the last two names, which should be separated by an ampersand.
+
+Example:
+
+list([ {name: 'Bart'}, {name: 'Lisa'}, {name: 'Maggie'} ])
+// returns 'Bart, Lisa & Maggie'
+
+list([ {name: 'Bart'}, {name: 'Lisa'} ])
+// returns 'Bart & Lisa'
+
+list([ {name: 'Bart'} ])
+// returns 'Bart'
+
+list([])
+// returns ''
+Note: all the hashes are pre-validated and will only contain A-Z, a-z, '-' and '.'.
+
+
+Test.assertEquals(list([{name: 'Bart'},{name: 'Lisa'},{name: 'Maggie'},{name: 'Homer'},{name: 'Marge'}]), 'Bart, Lisa, Maggie, Homer & Marge',
+"Must work with many names")
+Test.assertEquals(list([{name: 'Bart'},{name: 'Lisa'},{name: 'Maggie'}]), 'Bart, Lisa & Maggie',
+"Must work with many names")
+Test.assertEquals(list([{name: 'Bart'},{name: 'Lisa'}]), 'Bart & Lisa', 
+"Must work with two names")
+Test.assertEquals(list([{name: 'Bart'}]), 'Bart', "Wrong output for a single name")
+Test.assertEquals(list([]), '', "Must work with no names")
+
+
+LEVEL 6 kyu
+*/
